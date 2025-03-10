1a18d0f5c4c2d1f30983b76c491532919eb82272
Pascal <pascalrintareseketeti@gmail.com>
2025-03-11 01:05:46 +0300

    adding- review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const welcomeMessage = 'Welcome to Flatbook, ';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting = 'Welcome';
+console.log(shortGreeting);
+const shortGreeting = `Welcome, ${currentUser.charAt(0, 1)}!`;
